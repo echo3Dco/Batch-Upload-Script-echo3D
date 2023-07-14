1c45806e04e5dff2614ee37bf5f0f41aabee0baa
lawrencexli <lawrencexli2@gmail.com>
2023-07-14 16:20:57 -0400

    Added support for multiple file uploads

--- README.md
+++ README.md
@@ -22,8 +22,8 @@ This repository contains a publishable python script to batch upload models into
 ## Echo3D API Instructions
 
 1. Register for a FREE account at echo3D.
-3. Get your API key.
-4. Get your security key.
+2. Get your API key.
+3. Get your security key.
 
 ## Running Instructions
 
@@ -35,74 +35,75 @@ This repository contains a publishable python script to batch upload models into
 
 4. Run the command as follows:
    
-   `python upload.py [API_KEY] [SECURITY_KEY] [EMAIL] [TARGET_TYPE] [HOLOGRAM_TYPE] [BODY_ARGS]`
+   `python upload.py [API_KEY] [SECURITY_KEY] [EMAIL] [BODY_ARGS]`
 
    where
    
    * `[API_KEY]` is your Echo3D API key
    * `[SECURITY_KEY]` is your security key
    * `[EMAIL]` is the registered email of your echo3D account
-   * `[TARGET_TYPE]` is an integer specifying desired target type. Check out https://docs.echo3d.com/upload#target-type for detailed documentation.
-   * `[HOLOGRAM_TYPE]` is an integer specifying desired hologram asset type. Check out https://docs.echo3d.com/upload#asset-type for detailed documentation.
-   * `[BODY_ARGS]` is the path to your csv file that contains all other arguments needed for making a POST request to echo3D API. Check out the `template.csv`. The raw `csv` file must be separated by comma and contain exactly 2 columns similar to a key-value pair. 
+   * `[BODY_ARGS]` is the path to your csv file that contains all other arguments needed for making a POST request to echo3D API. Check out the `template.csv`. 
 
-5. If all arguments are filled correctly, the API will return status code of 200 and the entry json information of uploaded model. A code of 500 is an internal server exception, but it is most likely that your inputs may be incorrect. 
+5. If no errors are raised, an `out.json` file will be generated showing the status code and Echo3D API responded text for each uploaded entry. 
 
 ### Error handling
 
 This script can handle following errors:
  
- * Missing required arguments
- * Invalid file path for `csv` or model files (File not found error)
+ * Missing required arguments, duplicated arguments, and invalid arguments. 
+ * Invalid file path for `csv` and model files (File not found error)
  * Validity of values for longitude and latitude arguments. (Must be floating point)
- * Validity of target and hologram asset type. 
+ * Validity of specified target type. 
 
-If above errors are detected, the script will print the corresponding error message and stop the uploading process. For other errors that are not listed above, the script will either print out a status code of 400 or 500 from the echo3D API server, or raise a python exception. 
+If errors are detected above, the script will print the corresponding error message. If the error is not related to the CSV headers, it will also indicate the line in the CSV file that contains the error. Since the error check is done before the POST query, if an error is detected, no files will be uploaded. 
 
 ## About `template.csv`
 
-`template.csv` is the file that contains additional arguments needed to upload assets with a POST query. For example, if you choose to upload an image hologram (by specifying integer `1` in the hologram asset type), you will need to also specify the `file_image_hologram` argument. In this case, you need to specify such argument in the `template.csv` file. 
+`template.csv` is the file that contains additional arguments needed to upload assets with a POST query. These arguments are conditionally required based on the type of hologram asset file uploaded or the specified target type.
 
-To check out the required arguments for your specified target and hologram asset type, please go to https://docs.echo3d.com/upload. 
+Here is a list of valid and possibly required arguments: 
 
-By default, `template.csv` contains all additional arguments as keys. If an argument is not needed under your desired target or hologram asset type, just leave the value blank. Only fill out the values of needed arguments.  
+`['target_type', 'asset_file', 'url_image', 'url_video', 'file_image', 'text_geolocation', 'longitude', 'latitude', 'file_cvs']`
+
+By default, `template.csv` contains all these arguments as CSV headers. 
+
+The `template.csv` file supports multiple uploads. To do so, add rows where each row corresponds to a separate uploaded file. When adding a row for each file, if an argument is not required, simply leave it blank. 
 
 For arguments that require a file upload, you need to specify the path to your file. 
 
+This script can automatically calculate the hologram asset type based on the file extension of the uploaded file from the `asset_file` field. If you want to upload a model asset file, specify the filepath under `asset_file`. 
+
+For more details about these argument fields other than `asset_file`, please check out https://docs.echo3d.com/upload.
+
 ## Example run
 
-Here is a upload API examples using this script:
+Here is an upload API examples using this script:
 
 1. Uploading a model asset on a surface target from local storage:
-    * type is `upload` and file_model includes a file
-    * hologram_type is 2
-    * target_type is 2
 
-    `python upload.py abc-defg-123 abc123456789 yourname@gmail.com 2 2 template.csv`
+    `python upload.py abc-defg-123 abc123456789 yourname@gmail.com template.csv`
 
     where `template.csv` is as follows:
 
-    |  |  |
-    | ----------- | ----------- |
-    | url_image      |        |
-    | file_image   |         |
-    | text_geolocation   |         |
-    | longitude   |        |
-    | latitude   |        |
-    | url_video   |        |
-    | file_video   |         |
-    | file_image_hologram  |         |
-    | type   | upload       |
-    | file_model   | path/to/your/model/file        |
-    | source   |        |
-    | url   |       |
-    | name   |        |
-    | file_cvs   |        |
-    | edit_type |        |
-    | entryId   |        |
+   |target_type|asset_file                          |url_image|url_video|file_image|text_geolocation|longitude|latitude|file_cvs|
+   |-----------|------------------------------------|---------|---------|----------|----------------|---------|--------|--------|
+   |2          |/path/to/your/file/example1.glb     |         |         |          |                |         |        |        |
+   |2          |/path/to/your/file/example2.obj     |         |         |          |                |         |        |        |
+   |2          |/path/to/your/file/example3.mp4     |         |         |          |                |         |        |        |
+   |2          |/path/to/your/file/example4.png     |         |         |          |                |         |        |        |
+
 
     As shown above, arguments that are not needed are left blank. Only the required arguments are filled. 
 
+    A useful tip: It is possible to have the above CSV file as below:
 
+   |target_type|asset_file                          |
+   |-----------|------------------------------------|
+   |2          |/path/to/your/file/example1.glb     |
+   |2          |/path/to/your/file/example2.obj     |
+   |2          |/path/to/your/file/example3.mp4     |    
+   |2          |/path/to/your/file/example4.png     |
+
+    As shown, you can skip header arguments that are completely unused throughout the entire CSV file. 
 
 
--- upload.py
+++ upload.py
@@ -10,7 +10,10 @@ import csv
 from pathlib import Path
 import os
 from pprint import pprint
+import json
 
+VALID_ARGUMENTS = {'longitude', 'file_image', 'asset_file', 'file_cvs', 'url_image', 'target_type', 'latitude',
+                   'text_geolocation', 'url_video'}
 FILE_PATH_ARGS_NAME = {'file_image', 'asset_file'}
 UPLOAD_URL = 'https://api.echo3D.com/upload'
 
@@ -19,15 +22,18 @@ VIDEO_EXTENSION = {'mp4', 'mov'}
 IMAGE_EXTENSION = {'jpg', 'jpeg', 'png', 'gif', 'tiff', 'bmp', 'svg'}
 MODEL_EXTENSION = {'obj', 'gltf', 'glb', 'fbx', 'usdz', 'stl', 'blend', 'dae', 'sldprt', 'sldasm', 'step'}
 
+
 class TargetType(IntEnum):
     IMAGE_TARGET = 0
     GEOLOCATION_TARGET = 1
     BRICK_TARGET = 2
 
+
 class HologramType(IntEnum):
     VIDEO_HOLOGRAM = 0
     IMAGE_HOLOGRAM = 1
     MODEL_HOLOGRAM = 2
+
 
 def main():
     parser = argparse.ArgumentParser()
@@ -35,83 +41,114 @@ def main():
                         help='Your Echo3D API key')
     parser.add_argument('security_key', type=str,
                         help='Your Echo3D security key. Only if enabled through the security page')
-    parser.add_argument('email', type=str, 
+    parser.add_argument('email', type=str,
                         help='Your user email')
     parser.add_argument('body_args', type=str,
-                        help='A csv file containing all other arguments for POST body. Check out the example file template.csv for more details')
+                        help='Filepath to your csv file containing all other arguments for POST body. Check out the '
+                             'template.csv as an example')
 
     args = parser.parse_args()
-    data, files = build_body_args(args)
+    file_list = build_body_form_data(args)
+    if not file_list:
+        print("[WARNING] Your csv file is empty! No files are uploaded!")
+        return 0
 
-    if process_target_type(data, files) != 0:
-        print('Above errors need to be resolved in order to continue the batch upload process')
-        return -100
-
-    if process_hologram_type(data, files) != 0:
-        print('Above errors need to be resolved in order to continue the batch upload process')
-        return -200
-    
     print("===Body Form-Data Preview===")
-    pprint(data)
-    pprint(files)
+    pprint(file_list)
     print("============================")
 
-    r = post(data, files)
-    print("Status code:", r.status_code)
-    print("===API returned message below===")
-    print(r.text)
-    print("================================")
+    post(file_list)
+    print("All upload queries have been processed by Echo3D API. An out.txt file storing API returned status code and "
+          "results is generated")
 
     return 0
 
-"""
-Initialize arguments to build form-data for POST body
-"""
-def build_body_args(args):
-    data = {}
-    files = {}
-    data['key'] = args.api_key
-    data['secKey'] = args.security_key
-    data['email'] = args.email
-    
+
+# Build form-data for POST query
+def build_body_form_data(args):
     if not os.path.exists(Path(args.body_args)):
-        print("[CSV FILE NOT FOUND] Invalid filepath for body_args which is '%s': No such file or directory" % (args.body_args))
+        print("[CSV FILE NOT FOUND] Invalid filepath for body_args which is '%s': No such file or directory" %
+              args.body_args)
         exit(-2)
 
+    file_list = []
     with open(args.body_args) as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
-        for row in reader:
-            if len(row) != 2:
-                print("[CSV FORMAT ERROR] The number of columns in one of your rows in %s is %d. The csv file must contain only two columns: one for argument name and the second for values." % (args.body_args, len(row)))
+        header_list = next(reader)
+
+        # Check if all argument fields in csv header are valid. (Not empty and is a valid argument name)
+        for argument_field in header_list:
+            if not argument_field:
+                print(
+                    "[CSV FORMAT ERROR] One of your argument fields in csv header is empty. Please check your csv file")
                 exit(-3)
 
-            if row[0] in FILE_PATH_ARGS_NAME:
-                # Skip if the argument name key has no corresponding values
-                if not row[1]:
-                    continue
+            if argument_field not in VALID_ARGUMENTS:
+                print(
+                    "[CSV FORMAT ERROR] '%s' is not a valid argument name in csv header. Valid argument names are %s" %
+                    (argument_field, VALID_ARGUMENTS))
+                exit(-4)
 
-                try:
-                    files[row[0]] = open(Path(row[1]), 'rb')
-                except FileNotFoundError:
-                    print("[FILE PATH ERROR] File argument '%s' contains an invalid filepath which is '%s': No such file or directory" % (row[0], row[1]))
-                    exit(-4)
-            else:
-                # Skip if the argument name key has no corresponding values
-                if not row[1]:
-                    continue
+        # Check if there are duplicate argument fields in csv header. 
+        header_set = set(header_list)
+        if len(header_set) != len(header_list):
+            print("[CSV FORMAT ERROR] Duplicate argument fields in csv header exist. Please check your csv file")
+            exit(-5)
 
-                data[row[0]] = row[1]
+        # Begin reading the data
+        csv_line = 2
+        for row in reader:
+            data = {'key': args.api_key, 'secKey': args.security_key, 'email': args.email}
+            files = {}
 
-    return data, files
+            if len(row) != len(header_list):
+                print(
+                    "[CSV FORMAT ERROR] A row has different number of columns from header. Please check your csv file")
+                print("Above error was detected in line %d of your csv file. You need to resolve it before continuing "
+                      "the batch upload process" % csv_line)
+                exit(-6)
 
-"""
-Process target type. 
-Handles all target_type related errors. 
-"""
+            for i in range(len(row)):
+                if header_list[i] in FILE_PATH_ARGS_NAME:
+                    # Skip if the argument name key has no corresponding values
+                    if not row[i]:
+                        continue
+
+                    try:
+                        files[header_list[i]] = open(Path(row[i]), 'rb')
+                    except FileNotFoundError:
+                        print(
+                            "[FILE PATH ERROR] File argument '%s' contains an invalid filepath which is '%s': No such "
+                            "file or directory" % (header_list[i], row[i]))
+                        print(
+                            "Above error was detected in line %d of your csv file. You need to resolve it before "
+                            "continuing the batch upload process" % csv_line)
+                        exit(-7)
+
+                else:
+                    # Skip if the argument name key has no corresponding values
+                    if not row[i]:
+                        continue
+
+                    data[header_list[i]] = row[i]
+
+            if process_target_type(data, files) != 0 or process_hologram_type(data, files) != 0:
+                print("Above error was detected in line %d of your csv file. You need to resolve it before continuing "
+                      "the batch upload process" % csv_line)
+                exit(-8)
+
+            file_list.append({'data': data, 'files': files})
+            csv_line += 1
+
+    return file_list
+
+
+# Process target type.
+# Handles all target_type related errors.
 def process_target_type(data, files):
     # Check if target_type are specified correctly.
     if 'target_type' not in data:
-        print('[BODY ARGS ERROR] Missing target_type. Please check your csv file')
+        print("[BODY ARGS ERROR] Missing target_type. Please check your csv file")
         return -10
     else:
         # Check the validity of hologram_type value
@@ -120,7 +157,9 @@ def process_target_type(data, files):
             data['target_type'] = int(data['target_type'])
             target_type = TargetType(data['target_type'])
         except ValueError:
-            print('[TARGET TYPE ERROR] Invalid value for target_type. Please check upload documentation for a list of appropriate values!')
+            print(
+                "[TARGET TYPE ERROR] Invalid value for target_type. Please check upload documentation for a list of "
+                "appropriate values!")
             return -11
 
     if target_type == TargetType.IMAGE_TARGET:
@@ -128,17 +167,25 @@ def process_target_type(data, files):
             print("[IMAGE TARGET ERROR] Either url_image or file_image must be specified in your csv file")
             return -20
         elif 'url_image' in data and 'file_image' in files:
-            print("[IMAGE TARGET ERROR] You cannot specify both url_image and file_image. You must only specify one of them in your csv file")
+            print(
+                "[IMAGE TARGET ERROR] You cannot specify both url_image and file_image. You must only specify one of "
+                "them in your csv file")
             return -21
-        
+
     if target_type == TargetType.GEOLOCATION_TARGET:
         if not check_coordinates(data) and 'text_geolocation' not in data:
-            print("[GEOLOCATION TARGET ERROR] Either text_geolocation or coordinate info (longitude and latitude) must be specified in your csv file. Hint: For coordinate info did you specify both longitude and latitude?")
+            print(
+                "[GEOLOCATION TARGET ERROR] Either text_geolocation or coordinate info (longitude and latitude) must "
+                "be specified in your csv file. For coordinate info you need to specify both longitude and "
+                "latitude")
             return -22
         elif check_coordinates(data) and 'text_geolocation' in data:
-            print("[GEOLOCATION TARGET ERROR] You cannot specify both text_geolocation and coordinate info (longitude and latitude). You must only specify one of them in your csv file. Hint: For coordinate info did you specify both longitude and latitude?")
+            print(
+                "[GEOLOCATION TARGET ERROR] You cannot specify both text_geolocation and coordinate info (longitude "
+                "and latitude). You must only specify one of them in your csv file. For coordinate info you "
+                "need to specify both longitude and latitude")
             return -23
-        
+
         if check_coordinates(data):
             try:
                 data['longitude'] = float(data['longitude'])
@@ -146,43 +193,46 @@ def process_target_type(data, files):
             except ValueError:
                 print("[GEOLOCATION TARGET ERROR] Longitude and latitude information must be in float")
                 return -24
-            
+
     return 0
 
-"""
-Automatically calculate the hologram type based on the extension of uploaded file.
-Automatically assign filepath to respective keyword based on calculated hologram type.
-Handles all hologram_type related errors. 
-"""
-def process_hologram_type(data, files):        
+
+# Automatically calculate the hologram type based on the extension of uploaded file.
+# Automatically assign filepath to respective keyword based on calculated hologram type.
+# Handles all hologram_type related errors.
+def process_hologram_type(data, files):
     if 'asset_file' in files and 'url_video' in data:
-        print("[BODY ARGS ERROR] You cannot specify both asset_file and url_video. You must specify only one of them in your csv file")
+        print(
+            "[BODY ARGS ERROR] You cannot specify both asset_file and url_video. You must specify only one of them in "
+            "your csv file")
         return -30
-    
+
     if 'asset_file' not in files and 'url_video' not in data:
-        print("[BODY ARGS ERROR] Missing asset_file or url_video argument. You need to specify one of them in your csv file")
+        print(
+            "[BODY ARGS ERROR] Missing asset_file or url_video. You must specify one of them in your csv file")
         return -31
-    
+
     if 'asset_file' in files:
         _, file_extension = os.path.splitext(files['asset_file'].name)
         file_extension = file_extension.replace('.', '')
-        
+
         if not file_extension:
-            print('[FILE EXTENSION ERROR] Missing file extension. Please check your file')
+            print("[FILE EXTENSION ERROR] Missing file extension. Please check your file")
             return -40
+
         hologram_type = calculate_hologram_type(file_extension)
         if hologram_type == -1:
-            print("[FILE EXTENSION ERROR] File extension %s is not supported" % (file_extension))
+            print("[FILE EXTENSION ERROR] File extension '%s' is not supported" % file_extension)
             return -41
 
         data['hologram_type'] = int(hologram_type)
 
         if hologram_type == HologramType.VIDEO_HOLOGRAM:
             files['file_video'] = files['asset_file']
-            
+
         if hologram_type == HologramType.IMAGE_HOLOGRAM:
             files['file_image_hologram'] = files['asset_file']
-            
+
         if hologram_type == HologramType.MODEL_HOLOGRAM:
             files['file_model'] = files['asset_file']
             data['type'] = 'upload'
@@ -192,14 +242,14 @@ def process_hologram_type(data, files):
     elif 'url_video' in data:
         hologram_type = HologramType.VIDEO_HOLOGRAM
         data['hologram_type'] = int(hologram_type)
-        
+
     return 0
 
-"""
-Check if both longitude and latitude are specified for coordinate information
-"""
+
+# Check if both longitude and latitude are specified for coordinate information
 def check_coordinates(data):
     return 'longitude' in data and 'latitude' in data
+
 
 def calculate_hologram_type(file_extension):
     if file_extension in VIDEO_EXTENSION:
@@ -211,11 +261,23 @@ def calculate_hologram_type(file_extension):
     else:
         return -1
 
-"""
-Invoke Echo3D API for POST request
-"""
-def post(data, files):
-    return requests.post(UPLOAD_URL, data=data, files=files)
+
+# Invoke Echo3D API for POST request
+def post(file_list):
+    results = []
+    for form_data in file_list:
+        if form_data['files'] == {}:
+            r = requests.post(UPLOAD_URL, data=form_data['data'])
+        else:
+            r = requests.post(UPLOAD_URL, data=form_data['data'], files=form_data['files'])
+
+        result = {'status_code': r.status_code, 'response_text': r.text}
+        results.append(result)
+
+    json_dict = json.dumps(results)
+    with open("out.json", "w") as outfile:
+        outfile.write(json_dict)
+
 
 if __name__ == '__main__':
     main()
