2a462168bee3b4863610806e8b363649318caaa2
lawrencexli <lawrencexli2@gmail.com>
2023-07-14 12:45:56 -0400

    Automatically calculate hologram type based on file extension

--- upload.py
+++ upload.py
@@ -4,27 +4,27 @@ Build a publishable script to batch upload models into echo3D
 """
 
 import argparse
-from enum import Enum
+from enum import IntEnum
 import requests
 import csv
 from pathlib import Path
 import os
 from pprint import pprint
 
-FILE_PATH_ARGS_NAME = {'file_image', 'file_video', 'file_image_hologram', 'file_model', 'png_path'}
-SUPPORTED_SOURCES = {'Sketchfab', 'Poly'}
+FILE_PATH_ARGS_NAME = {'file_image', 'asset_file'}
 UPLOAD_URL = 'https://api.echo3D.com/upload'
 
+# Lists of supported file extensions for each hologram asset type
 VIDEO_EXTENSION = {'mp4', 'mov'}
 IMAGE_EXTENSION = {'jpg', 'jpeg', 'png', 'gif', 'tiff', 'bmp', 'svg'}
 MODEL_EXTENSION = {'obj', 'gltf', 'glb', 'fbx', 'usdz', 'stl', 'blend', 'dae', 'sldprt', 'sldasm', 'step'}
 
-class TargetType(Enum):
+class TargetType(IntEnum):
     IMAGE_TARGET = 0
     GEOLOCATION_TARGET = 1
     BRICK_TARGET = 2
 
-class HologramType(Enum):
+class HologramType(IntEnum):
     VIDEO_HOLOGRAM = 0
     IMAGE_HOLOGRAM = 1
     MODEL_HOLOGRAM = 2
@@ -41,16 +41,20 @@ def main():
                         help='A csv file containing all other arguments for POST body. Check out the example file template.csv for more details')
 
     args = parser.parse_args()
-    
     data, files = build_body_args(args)
+
+    if process_target_type(data, files) != 0:
+        print('Above errors need to be resolved in order to continue the batch upload process')
+        return -100
+
+    if process_hologram_type(data, files) != 0:
+        print('Above errors need to be resolved in order to continue the batch upload process')
+        return -200
+    
     print("===Body Form-Data Preview===")
     pprint(data)
     pprint(files)
     print("============================")
-
-    if body_args_error_handle(data, files) != 0:
-        print('Above errors need to be resolved in order to continue the batch upload process')
-        return -100
 
     r = post(data, files)
     print("Status code:", r.status_code)
@@ -89,7 +93,7 @@ def build_body_args(args):
                 try:
                     files[row[0]] = open(Path(row[1]), 'rb')
                 except FileNotFoundError:
-                    print("[FILE PATH ERROR] File argument key '%s' in '%s' contains an invalid filepath which is '%s': No such file or directory" % (row[0], args.body_args, row[1]))
+                    print("[FILE PATH ERROR] File argument '%s' contains an invalid filepath which is '%s': No such file or directory" % (row[0], row[1]))
                     exit(-4)
             else:
                 # Skip if the argument name key has no corresponding values
@@ -101,93 +105,93 @@ def build_body_args(args):
     return data, files
 
 """
-Error handling for missing arguments
+Process target type. 
+Handles all target_type related errors. 
 """
-def body_args_error_handle(data, files):
+def process_target_type(data, files):
     # Check if target_type are specified correctly.
     if 'target_type' not in data:
-        print('[BODY ARGS ERROR] Missing target type. Please check your csv file')
-        return -50
+        print('[BODY ARGS ERROR] Missing target_type. Please check your csv file')
+        return -10
     else:
         # Check the validity of hologram_type value
+        # Also try to parse the input raw string value into integer
         try:
-            target_type = TargetType(int(data['target_type']))
+            data['target_type'] = int(data['target_type'])
+            target_type = TargetType(data['target_type'])
         except ValueError:
-            print('[TARGET TYPE ERROR] Invalid value for target_type. Please check upload documentation for appropriate values!')
-            return -51
-
-    # Check if hologram_type are specified correctly.
-    if 'hologram_type' not in data:
-        print('[BODY ARGS ERROR] Missing hologram type. Please check your csv file')
-        return -52
-    else:
-        # Check the validity of hologram_type value
-        try:
-            hologram_type = HologramType(int(data['hologram_type']))
-        except ValueError:
-            print('[HOLOGRAM TYPE ERROR] Invalid value for hologram_type. Please check upload documentation for appropriate values!')
-            return -53
+            print('[TARGET TYPE ERROR] Invalid value for target_type. Please check upload documentation for a list of appropriate values!')
+            return -11
 
     if target_type == TargetType.IMAGE_TARGET:
         if 'url_image' not in data and 'file_image' not in files:
             print("[IMAGE TARGET ERROR] Either url_image or file_image must be specified in your csv file")
-            return -10
+            return -20
         elif 'url_image' in data and 'file_image' in files:
             print("[IMAGE TARGET ERROR] You cannot specify both url_image and file_image. You must only specify one of them in your csv file")
-            return -11
+            return -21
         
     if target_type == TargetType.GEOLOCATION_TARGET:
         if not check_coordinates(data) and 'text_geolocation' not in data:
             print("[GEOLOCATION TARGET ERROR] Either text_geolocation or coordinate info (longitude and latitude) must be specified in your csv file. Hint: For coordinate info did you specify both longitude and latitude?")
-            return -12
+            return -22
         elif check_coordinates(data) and 'text_geolocation' in data:
             print("[GEOLOCATION TARGET ERROR] You cannot specify both text_geolocation and coordinate info (longitude and latitude). You must only specify one of them in your csv file. Hint: For coordinate info did you specify both longitude and latitude?")
-            return -13
+            return -23
         
         if check_coordinates(data):
             try:
                 data['longitude'] = float(data['longitude'])
                 data['latitude'] = float(data['latitude'])
             except ValueError:
-                print("[GEOLOCATION TARGET ERROR] Longitude and latitude information must be float")
-                return -14
+                print("[GEOLOCATION TARGET ERROR] Longitude and latitude information must be in float")
+                return -24
+            
+    return 0
 
-    if hologram_type == HologramType.VIDEO_HOLOGRAM:
-        if 'url_video' not in data and 'file_video' not in files:
-            print("[VIDEO HOLOGRAM ERROR] Either url_video or file_video must be specified in your csv file")
-            return -20
-        if 'url_video' in data and 'file_video' in files:
-            print("[VIDEO HOLOGRAM ERROR] You cannot specify both url_video and file_video. You must only specify one of them in your csv file")
-            return -21
+"""
+Automatically calculate the hologram type based on the extension of uploaded file.
+Automatically assign filepath to respective keyword based on calculated hologram type.
+Handles all hologram_type related errors. 
+"""
+def process_hologram_type(data, files):        
+    if 'asset_file' in files and 'url_video' in data:
+        print("[BODY ARGS ERROR] You cannot specify both asset_file and url_video. You must specify only one of them in your csv file")
+        return -30
+    
+    if 'asset_file' not in files and 'url_video' not in data:
+        print("[BODY ARGS ERROR] Missing asset_file or url_video argument. You need to specify one of them in your csv file")
+        return -31
+    
+    if 'asset_file' in files:
+        _, file_extension = os.path.splitext(files['asset_file'].name)
+        file_extension = file_extension.replace('.', '')
         
-    if hologram_type == HologramType.IMAGE_HOLOGRAM:
-        if 'file_image_hologram' not in files:
-            print("[IMAGE HOLOGRAM ERROR] You must specify file_image_hologram in your csv file")
-            return -22
-        
-    if hologram_type == HologramType.MODEL_HOLOGRAM:
-        if 'type' not in data:
-            print("[MODEL HOLOGRAM ERROR] You must specify 'type' argument in your csv file")
-            return -23
-        if data['type'] not in {'upload', 'search'}:
-            print("[MODEL HOLOGRAM ERROR] 'type' argument only accepts 'upload' or 'search' values")
-            return -24
-        if data['type'] == 'upload' and 'file_model' not in files:
-            print("[MODEL HOLOGRAM ERROR] You must specify file_model argument as the filepath of your model if the upload option is chosen")
-            return -25
-        if data['type'] == 'search' and ('source' not in data or 'name' not in data):
-            print("[MODEL HOLOGRAM ERROR] You must specify both the source argument and name argument in your csv file if the search option is chosen")
-            return -26
-        if data['type'] == 'search' and data['source'] not in SUPPORTED_SOURCES:
-            print("[MODEL HOLOGRAM ERROR] Specified source %s is not supported. Currently only Sketchfab is supported." % (data['source']))
-            return -27
-        # Google Poly was shut down and therefore it is impossible to retrieve any models from it. 
-        if data['type'] == 'search' and data['source'] == 'Poly':
-            print("[MODEL HOLOGRAM ERROR] Google Poly is no longer supported as the server was shut down on June 30, 2021")
-            return -28
-        if data['type'] == 'search' and data['source'] == 'Sketchfab' and 'url' not in data:
-            print("[MODEL HOLOGRAM ERROR] You must specify the url argument in your csv file, which is the Sketchfab API URL redirecting to the model. Should be in the form of https://api.sketchfab.com/v3/models/<ID>/download")
-            return -29
+        if not file_extension:
+            print('[FILE EXTENSION ERROR] Missing file extension. Please check your file')
+            return -40
+        hologram_type = calculate_hologram_type(file_extension)
+        if hologram_type == -1:
+            print("[FILE EXTENSION ERROR] File extension %s is not supported" % (file_extension))
+            return -41
+
+        data['hologram_type'] = int(hologram_type)
+
+        if hologram_type == HologramType.VIDEO_HOLOGRAM:
+            files['file_video'] = files['asset_file']
+            
+        if hologram_type == HologramType.IMAGE_HOLOGRAM:
+            files['file_image_hologram'] = files['asset_file']
+            
+        if hologram_type == HologramType.MODEL_HOLOGRAM:
+            files['file_model'] = files['asset_file']
+            data['type'] = 'upload'
+
+        del files['asset_file']
+
+    elif 'url_video' in data:
+        hologram_type = HologramType.VIDEO_HOLOGRAM
+        data['hologram_type'] = int(hologram_type)
         
     return 0
 
@@ -205,9 +209,8 @@ def calculate_hologram_type(file_extension):
     elif file_extension in MODEL_EXTENSION:
         return HologramType.MODEL_HOLOGRAM
     else:
-        print('[FILE EXTENSION ERROR] File extension %s is not supported.' % (file_extension))
         return -1
-    a
+
 """
 Invoke Echo3D API for POST request
 """
